249ce2a91c31a1f14bf80638e95ba1c7217f201b
SOFTBOY <cntysoft@gmail.com>
2014-11-14 16:28:41 +0800

    为webos增加新的编辑器子类

---
+++ src/Component/CkEditor/Editor.js
@@ -0,0 +1,17 @@
+/*
+ * Cntysoft Cloud Software Team
+ * 
+ * @author SOFTBOY <cntysoft@163.com>
+ * @copyright  Copyright (c) 2010-2011 Cntysoft Technologies China Inc. <http://www.cntysoft.com>
+ * @license    http://www.cntysoft.com/license/new-bsd     New BSD License
+ */
+Ext.define('WebOs.Component.CkEditor.Editor', {
+   extend : 'Cntysoft.Component.CkEditor.Editor',
+   uploadRequestMeta : {
+      url : WebOs.Const.API_GATE_SYS,
+      meta : {
+         name : 'WebUploaderHandler',
+         method : 'process'
+      }
+   }
+});
