ceaa265f9190aba7bd3cc14680a1aa7b3f1d40b6
Changwang <chenyongwang1104@163.com>
2015-05-30 14:46:42 +0800

    添加七牛云存储到CkEditor

---
+++ src/Component/QiniuCkEditor/Editor.js
@@ -0,0 +1,20 @@
+/*
+ * Cntysoft Cloud Software Team
+ * 
+ * @author SOFTBOY <cntysoft@163.com>
+ * @copyright  Copyright (c) 2010-2011 Cntysoft Technologies China Inc. <http://www.cntysoft.com>
+ * @license    http://www.cntysoft.com/license/new-bsd     New BSD License
+ */
+Ext.define('WebOs.Component.QiniuCkEditor.Editor', {
+   extend : 'Cntysoft.Component.QiniuCkEditor.Editor',
+   requires : [
+      'WebOs.Kernel.Const'
+   ],
+   uploadRequestMeta : {
+      url : WebOs.Kernel.Const.API_GATE_SYS,
+      meta : {
+         name : 'WebUploaderHandler',
+         method : 'process'
+      }
+   }
+});
